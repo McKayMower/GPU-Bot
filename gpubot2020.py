7e184391d41da88d8f6624224b6d60e074abec1b
McKay Mower <mmower777@gmail.com>
2020-12-19 12:38:25 -0700

    Added checks to see if product is sold out or if you can add to cart.

--- gpubot2020.py
+++ gpubot2020.py
@@ -4,9 +4,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import bs4
 
-url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442'
+# actual gpu
+# url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442'
 
+# add to cart test
+# url = 'https://www.bestbuy.com/site/amd-ryzen-7-3700x-3rd-generation-8-core-16-thread-3-6-ghz-4-4-ghz-max-boost-socket-am4-unlocked-desktop-processor/6356277.p?skuId=6356277'
+# sold out test
+url = 'https://www.bestbuy.com/site/msi-aegis-rs-gaming-desktop-intel-core-i7-10700kf-16gb-memory-nvidia-geforce-rtx-3080-1tb-ssd-black-black/6439310.p?skuId=6439310'
 
 # creates a web driver with my personal google chrome options and returns it
 def make_driver():
@@ -26,9 +32,23 @@ def find_card(driver):
     driver.get(url)
 
     while True:
-        html = driver.page_source
-        time.sleep(1)
-        print(html)
+        markup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+        # print(markup)
+        # driver.close()
+        try:
+            add_to_cart = markup.find('button', {'class': 'btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button'})
+            sold_out = markup.find('button', {'class': 'btn btn-disabled btn-lg btn-block add-to-cart-button'})
+            if add_to_cart:
+                print(f'found button:', add_to_cart.get_text())
+            elif sold_out:
+                print(f'found button:', sold_out.get_text())
+                time.sleep(5)
+                driver.refresh()
+
+        # if no card was found, wait 5 seconds, refresh page, try again.
+        finally:
+            time.sleep(5)
+            driver.refresh()
 
 
 
