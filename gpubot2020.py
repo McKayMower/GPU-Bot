976c4d81c52a7a2f8a2882cc26d73691d4992c51
McKay Mower <mmower777@gmail.com>
2020-12-19 00:46:50 -0700

    added main method implementation, made a webdriver that uses my chrome options, started to parse html from website

--- gpubot2020.py
+++ gpubot2020.py
@@ -1,2 +1,37 @@
 # this file is a bot that buys a gpu
-print('testing')
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442'
+
+
+# creates a web driver with my personal google chrome options and returns it
+def make_driver():
+    # open a new instance of chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-data-dir=C:\\Users\\mmowe\\AppData\\Local\\Google\\Chrome\\User Data')
+
+    # might be able to use this: options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    driver = webdriver.Chrome('C:\\Users\\mmowe\\PycharmProjects\\chromedriver.exe', options=options)
+
+    return driver
+
+
+# finds the gpu from the link
+def find_card(driver):
+    # open url in chrome
+    driver.get(url)
+
+    while True:
+        html = driver.page_source
+        time.sleep(1)
+        print(html)
+
+
+
+if __name__ == '__main__':
+    driver = make_driver()
+    find_card(driver)
